25f72b2a5919f3041e1c98904cfe0bdd8c49e487
Koushik Reddy <koushiksandi@gmail.com>
2026-08-03 04:49:35 +0000

    Updated products related models with the latest changes

--- models/gold/dim_customers.sql
+++ models/gold/dim_customers.sql
@@ -41,27 +41,27 @@ current_open as (
 change_scd2 as (
     select
         s.*,
-        c.customer_sk as existing_sk,
-        c.scd2_hash as existing_scd2_hash,
-        c.full_name as existing_full_name,
-        c.email as existing_email,
-        c.phone as existing_phone,
-        c.city as existing_city,
-        c.country as existing_country,
-        c.state as existing_state,
-        c.zip as existing_zip,
-        c.is_active as existing_activity,
-        c.loyalty_tier as existing_loyalty_tier,
-        c.priority as existing_priority,
-        c.signup_date as existing_signup_date
+        c.customer_sk       as existing_sk,
+        c.scd2_hash         as existing_scd2_hash,
+        c.full_name         as existing_full_name,
+        c.email             as existing_email,
+        c.phone             as existing_phone,
+        c.city              as existing_city,
+        c.country           as existing_country,
+        c.state             as existing_state,
+        c.zip               as existing_zip,
+        c.is_active         as existing_activity,
+        c.loyalty_tier      as existing_loyalty_tier,
+        c.priority          as existing_priority,
+        c.signup_date       as existing_signup_date
     from source_data s
     left join current_open c
-        on c.customer_id = s.customer_id
+        on c.customer_id    = s.customer_id
 ),
 
 rows_to_close as (
     select
-        existing_sk as customer_sk,
+        existing_sk         as customer_sk,
         customer_id,
         full_name,
         email,
@@ -75,12 +75,12 @@ rows_to_close as (
         priority,
         signup_Date,
         scd2_hash,
-        loaded_at as effective_start_date,
+        loaded_at           as effective_start_date,
         current_timestamp() as effective_end_date,
         false as is_current
     from change_scd2
-    where scd2_hash != existing_scd2_hash
-        and existing_sk is not null
+    where scd2_hash         != existing_scd2_hash
+        and existing_sk     is not null
 ),
 
 rows_to_open as (
@@ -99,17 +99,17 @@ rows_to_open as (
         priority,
         signup_Date,
         scd2_hash,
-        loaded_at as effective_start_date,
+        loaded_at           as effective_start_date,
         null::timestamp_ntz as effective_end_date,
         true as is_current
     from change_scd2
-    where scd2_hash != existing_scd2_hash
-        and existing_sk is not null
+    where scd2_hash         != existing_scd2_hash
+        and existing_sk     is not null
 ),
 
 update_rows_scd1 as (
     select
-        existing_sk as customer_sk,
+        existing_sk         as customer_sk,
         customer_id,
         full_name,
         email,
@@ -128,20 +128,20 @@ update_rows_scd1 as (
         true as is_current
     from change_scd2
     where 
-        scd2_hash = existing_scd2_hash
+        scd2_hash           = existing_scd2_hash
         and 
-        existing_sk is not null
+        existing_sk         is not null
         and (
-            email       is distinct from existing_email
-        or phone        is distinct from existing_phone
-        or city         is distinct from existing_city
-        or country      is distinct from existing_country
-        or state        is distinct from existing_state
-        or zip          is distinct from existing_zip
-        or is_active    is distinct from existing_activity
-        or loyalty_tier is distinct from existing_loyalty_tier
-        or priority     is distinct from existing_priority
-        or signup_date  is distinct from existing_signup_date
+            email           is distinct from existing_email
+        or phone            is distinct from existing_phone
+        or city             is distinct from existing_city
+        or country          is distinct from existing_country
+        or state            is distinct from existing_state
+        or zip              is distinct from existing_zip
+        or is_active        is distinct from existing_activity
+        or loyalty_tier     is distinct from existing_loyalty_tier
+        or priority         is distinct from existing_priority
+        or signup_date      is distinct from existing_signup_date
         )
 ),
 
@@ -161,12 +161,12 @@ new_rows as (
         priority,
         signup_Date,
         scd2_hash,
-        loaded_at as effective_start_date,
+        loaded_at           as effective_start_date,
         null::timestamp_ntz as effective_end_date,
         true as is_current
     from change_scd2
-    where scd2_hash != existing_scd2_hash
-        and existing_sk is null
+    where scd2_hash         != existing_scd2_hash
+        and existing_sk     is null
 ),
 
 final as (
@@ -272,7 +272,7 @@ select
     priority,
     signup_Date,
     scd2_hash,
-    loaded_at as effective_start_date,
+    loaded_at           as effective_start_date,
     null::timestamp_ntz as effective_end_date,
     true as is_current
 from source_data
