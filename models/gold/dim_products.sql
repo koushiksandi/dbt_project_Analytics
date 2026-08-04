62e3728c7879c7b1988aff964c21471cbb75aac6
Koushik Reddy <koushiksandi@gmail.com>
2026-08-04 04:53:48 +0000

    change

--- models/gold/dim_products.sql
+++ models/gold/dim_products.sql
@@ -47,6 +47,23 @@ change_detection as (
         on s.product_id = c.product_id
 ),
 
+deletion_detection as (
+    select
+        s.*,
+        c.product_sk    as existing_surrogate_key,
+        c.category      as existing_category,
+        c.stock_status  as existing_stock_status,
+        c.name          as existing_name,
+        c.price         as existing_price,
+        c.rating        as existing_rating,
+        c.tags          as existing_tags,
+        c.scd2_hash     as existing_scd2_hash
+    from source_data s
+    right join current_open c
+        on s.product_id = c.product_id
+    where s.product_id is null
+),
+
 rows_to_add_new as (
     select
         {{ dbt_utils.generate_surrogate_key(['product_id', 'updated_at'])}} as product_sk,
@@ -123,6 +140,24 @@ rows_to_update_scd1 as (
         and scd2_hash               = existing_scd2_hash
 ),
 
+rows_deleted_at_source as (
+    select
+        existing_surrogate_key      as product_sk,
+        existing_product_id         as product_id,
+        existing_category           as category,
+        existing_stock_status       as stock_status,
+        existing_name               as name,
+        existing_price              as price,
+        existing_rating             as rating,
+        existing_tags               as tags,
+        existing_scd2_hash          as scd2_hash,
+        effective_start_date,
+        current_timestamp()         as effective_end_date,
+        false                       as is_current
+    from deletion_detection
+    where existing_surrogate_key    is not null
+), 
+
 final as (
     select
         product_sk,
@@ -183,6 +218,21 @@ final as (
         effective_end_date,
         is_current
     from rows_to_update_scd1
+    union all
+    select
+        product_sk,
+        product_id,
+        category,
+        stock_status,
+        name,
+        price,
+        rating,
+        tags,
+        scd2_hash,
+        effective_start_date,
+        effective_end_date,
+        is_current
+    from rows_deleted_at_source
 )
 select
     *
